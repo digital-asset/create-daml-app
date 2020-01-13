45032df309d812149994f73f6575537aa429c274
Rohan Jacob-Rao <rohanjr@gmail.com>
2020-01-13 16:14:20 -0500

    Use undefined instead of null for better chaining

--- ui/src/components/MainView.tsx
+++ ui/src/components/MainView.tsx
@@ -6,7 +6,7 @@ import { User } from '../daml/create-daml-app/User';
 import { Party } from '@digitalasset/daml-json-types';
 
 export type Props = {
-  myUser: User | null;
+  myUser: User | undefined;
   allUsers: User[];
   onAddFriend: (friend: Party) => Promise<boolean>;
   onRemoveFriend: (friend: Party) => Promise<void>;
