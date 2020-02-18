766b1c26ef47f7ba15d1f0181a645f1296464300
Martin Huschenbett <martin.huschenbett@posteo.me>
2020-02-18 19:49:30 +0100

    Sketch of the daml2ts target state

---
+++ daml.js/create-daml-app/packageId.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = '3aa5962a30ed7c9b0e90b6d8c8a2f56aa80f90846caaea4500b3507a7df84ed3';
+//# sourceMappingURL=packageId.js.map
