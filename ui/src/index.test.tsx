cba42e4ee2d3296371637380a5e1b591a6e70109
Rohan Jacob-Rao <rohanjr@gmail.com>
2020-03-04 16:51:02 -0500

    Clean up code and remove commented code

--- ui/src/index.test.tsx
+++ ui/src/index.test.tsx
@@ -4,12 +4,9 @@ import waitOn from 'wait-on';
 import Ledger from '@daml/ledger';
 import { User } from '@daml2ts/create-daml-app/lib/create-daml-app-0.1.0/User';
 import { wsBaseUrl } from './config';
-import Credentials, { computeCredentials } from './Credentials';
+import { computeCredentials } from './Credentials';
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import puppeteer from "puppeteer";
-
+const DAR_PATH = '.daml/dist/create-daml-app-0.1.0.dar';
 const LEDGER_ID = 'create-daml-app-sandbox';
 const APPLICATION_ID = 'create-daml-app';
 const SANDBOX_PORT = 6865;
@@ -40,44 +37,16 @@ afterAll(() => {
 
 const fail = () => expect(false).toBeTruthy();
 
-// it('run daml start and shut it down', async () => {
-//   const startArgs = [ "start", "--open-browser=no", "--start-navigator=no", "--sandbox-option=--wall-clock-time", "--sandbox-option='--ledgerid=create-daml-app-sandbox'" ];
-//   const startCmd = ["daml"].concat(startArgs).join(' ');
-//   const env = Object.assign(process.env, {PATH: process.env.HOME + '/.daml/bin:' + process.env.PATH});
-//   const startOpts = { cwd: '..', env }; // run in root dir with extended path
-//   startProc = exec(startCmd, startOpts, (error, stdout, stderr) => {
-//     if (error && !error.killed) {
-//       console.error(error);
-//       fail();
-//     }
-//     if (stderr) {
-//       console.error(stderr);
-//       fail();
-//     }
-//     if (stdout) {
-//       console.error(stdout);
-//       fail();
-//     }
-//   });
-//   await waitOn({resources: [`tcp:localhost:${SANDBOX_PORT}`]});
-//   await waitOn({resources: [`tcp:localhost:${JSON_API_PORT}`]});
+// Extend timeout to allow sandbox and json api server to start running
+jest.setTimeout(20_000);
 
-  // Connect browser
-  // let browser = await puppeteer.launch();
-  // const page = await browser.newPage();
-  // await page.goto(`http://localhost:3000`);
-// });
-
-// Default timeout is too short to allow both sandbox and json api server to start running
-jest.setTimeout(10_000);
 it('starts sandbox and json api server', async () => {
   // Set up enviroment to run daml assistant commands
   const env = Object.assign(process.env, {PATH: process.env.HOME + '/.daml/bin:' + process.env.PATH});
   const cmdOpts = { cwd: '..', env }; // run in root dir with extended path
 
   // Start sandbox
-  const sandboxArgs = [ 'sandbox', '--wall-clock-time', `--port=${SANDBOX_PORT}`, `--ledgerid=${LEDGER_ID}` ];
-  const sandboxCmd = ['daml'].concat(sandboxArgs).join(' ');
+  const sandboxCmd = `daml sandbox --wall-clock-time --port=${SANDBOX_PORT} --ledgerid=${LEDGER_ID} ${DAR_PATH}`;
   sandboxProc = exec(sandboxCmd, cmdOpts, (error, stdout, stderr) => {
     if (error && !error.killed) {
       console.error(error);
@@ -86,9 +55,6 @@ it('starts sandbox and json api server', async () => {
     if (stderr) {
       console.error(stderr);
       fail();
-    }
-    if (stdout) {
-      console.error(stdout);
     }
   });
   await waitOn({resources: [`tcp:localhost:${SANDBOX_PORT}`]});
@@ -104,22 +70,11 @@ it('starts sandbox and json api server', async () => {
       console.error(stderr);
       fail();
     }
-    if (stdout) {
-      console.error(stdout);
-    }
   });
   await waitOn({resources: [`tcp:localhost:${JSON_API_PORT}`]});
 
-  console.log("Ports open!")
-  await queryUser();
+  await createAndLookUpUser();
 });
-
-const queryUser = async () => {
-  const credentials = computeCredentials('Alice');
-  const ledger = new Ledger({token: credentials.token, httpBaseUrl: undefined, wsBaseUrl});
-  const events = await ledger.query(User);
-  expect(events).toEqual([]);
-}
 
 const createAndLookUpUser = async () => {
   const credentials = computeCredentials('Alice');
@@ -129,4 +84,6 @@ const createAndLookUpUser = async () => {
   const userContract1 = await ledger.create(User, user);
   const userContract2 = await ledger.lookupByKey(User, credentials.party);
   expect(userContract1).toEqual(userContract2);
+  const events = await ledger.query(User);
+  expect(events[0].contractId).toEqual(userContract1.contractId);
 }
