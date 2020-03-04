3581cdac000692118d8c3d3726ce516c2f7e972d
Robin Krom <robin@digitalasset.com>
2020-03-04 13:45:25 +0100

    only reload on successful dar compilation

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,11 @@
 DAR=.daml/dist/create-daml-app-0.1.0.dar
 
 daml build -o ${DAR}
-daml codegen ts ${DAR} -o daml-ts/src
-pushd daml-ts
-yarn build
-popd
-daml ledger upload-dar ${DAR}
+
+if [ $? -eq 0 ]; then
+  daml codegen ts ${DAR} -o daml-ts/src
+  pushd daml-ts
+  yarn build
+  popd
+  daml ledger upload-dar ${DAR}
+fi
