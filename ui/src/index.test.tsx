63830057adaf878ddfffc3db928cf253da5912dd
Rohan Jacob-Rao <rohanjr@gmail.com>
2020-03-04 11:10:12 -0500

    Get daml start to run (I think)

--- ui/src/index.test.tsx
+++ ui/src/index.test.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { ChildProcess, spawn } from 'child_process';
-import { lstat } from 'fs';
-// import waitOn from 'wait-on';
+import { ChildProcess, spawn, exec, ExecOptions } from 'child_process';
 
-const LEDGER_ID = 'daml2ts-tests';
-
+const LEDGER_ID = 'create-daml-app-sandbox';
 const SANDBOX_PORT = 6865;
 const JSON_API_PORT = 7575;
 
 let sandboxProcess: ChildProcess | undefined = undefined;
 let jsonApiProcess: ChildProcess | undefined = undefined;
 let startProc: ChildProcess | undefined = undefined;
-let ls: ChildProcess | undefined = undefined;
 
 const getEnv = (variable: string): string => {
   const result = process.env[variable];
@@ -23,42 +19,55 @@ const getEnv = (variable: string): string => {
   return result;
 }
 
-// const spawnJvmAndWaitOnPort = async (jar: string, args: string[], port: number): Promise<ChildProcess> => {
-//   const java = getEnv('JAVA');
-//   const proc = spawn(java, ['-jar', jar, ...args], {stdio: "inherit",});
-//   await waitOn({resources: [`tcp:localhost:${port}`]})
-//   return proc;
-// }
-
 beforeAll(async () => {
   // const rootDir = getEnv('PWD') + '/..';
   // const darPath = rootDir + '/.daml/dist/create-daml-app-0.1.0.dar';
-  const startArgs = [ "start", "--open-browser=no", "--start-navigator=no", "--sandbox-option=--wall-clock-time", "--sandbox-option='--ledgerid=create-daml-app-sandbox'" ];
-  const env = Object.assign(process.env, {PATH: process.env.HOME + '/.daml/bin:' + process.env.PATH});
-  console.log(env.PATH);
-  const startOpts = { cwd: '..', env }; // run in root dir
-  startProc = spawn('daml', ['fail'], startOpts)
-  startProc.on('error', (err) => console.error('daml start failed with error:' + JSON.stringify(err)));
-  startProc.stderr.on('data', (data) => {
-    console.error(`stdout: ${data}`);
-  });
+  // const startArgs = [ "start", "--open-browser=no", "--start-navigator=no", "--sandbox-option=--wall-clock-time", "--sandbox-option='--ledgerid=create-daml-app-sandbox'" ];
+  // const env = Object.assign(process.env, {PATH: process.env.HOME + '/.daml/bin:' + process.env.PATH});
+  // console.log(env.PATH);
+  // const startOpts: SpawnOptions = { cwd: '..', env, stdio: 'pipe' }; // run in root dir
+  // startProc = spawn('dam', ['fail'], startOpts)
+  // startProc.on('error', (err) => console.error('daml start failed with error:' + JSON.stringify(err)));
+  // startProc.stderr.on('data', (data) => {
+  //   console.error(`stdout: ${data}`);
+  // });
   // const ls = spawn('ls', [], );
   // ls.stdout.on('data', (data) => {
   //   console.log(`stdout: ${data}`);
   // });
-  console.log('Sandbox up');
-  console.log('JSON API up');
+  // console.log('Sandbox up');
+  // console.log('JSON API up');
 });
 
 afterAll(() => {
   // if (ls) {
   //   ls.kill();
   // }
-  if (startProc) {
-    startProc.kill("SIGTERM");
-  }
-  console.log('Killed daml start');
+  // if (startProc) {
+  //   startProc.kill("SIGTERM");
+  //   console.log('Killed daml start');
+  // }
 });
 
-test('create + fetch & exercise', async () => {
+const fail = () => expect(false).toBeTruthy();
+
+it('starts sandbox and json api server', () => {
+  const startArgs = [ "start", "--open-browser=no", "--start-navigator=no", "--sandbox-option=--wall-clock-time", "--sandbox-option='--ledgerid=create-daml-app-sandbox'" ];
+  const startCmd = ["daml"].concat(startArgs).join(' ');
+  const env = Object.assign(process.env, {PATH: process.env.HOME + '/.daml/bin:' + process.env.PATH});
+  const startOpts = { cwd: '..', env }; // run in root dir with extended path
+  startProc = exec(startCmd, startOpts, (error, stdout, stderr) => {
+    if (error) {
+      console.error(error);
+      fail();
+    }
+    if (stderr) {
+      console.error(stderr);
+      fail();
+    }
+    if (stdout) {
+      console.error(stdout);
+      fail();
+    }
+  });
 });
