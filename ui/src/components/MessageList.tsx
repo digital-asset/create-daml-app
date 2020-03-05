49232a3f59c2da0e1f03188455d5fe6d1c08ed7a
Martin Huschenbett <martin.huschenbett@posteo.me>
2020-03-05 18:40:19 +0100

    Show incoming and outgoing messages
    Also use the contract id of each message as its key and polish the
    look of the messages a bit.

--- ui/src/components/MessageList.tsx
+++ ui/src/components/MessageList.tsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import { List, ListItem } from 'semantic-ui-react';
 import { Message } from '@daml2ts/create-daml-app/lib/create-daml-app-0.1.0/User';
-import { useParty, useStreamQuery } from '@daml/react';
+import { useStreamQuery } from '@daml/react';
 
 /**
  * React component displaying the list of messages for the current user.
  */
 const MessageList: React.FC = () => {
-  const username = useParty();
-  const messagesResult = useStreamQuery(Message, () => ({receiver: username}), []);
-  const messages = messagesResult.contracts.map(message => message.payload);
-
-  const showMessage = (message: Message): string => {
-    return (message.sender + ": " + message.content);
-  }
+  const messagesResult = useStreamQuery(Message);
 
   return (
     <List relaxed>
-      {messages.map(message => <ListItem>{showMessage(message)}</ListItem>)}
+      {messagesResult.contracts.map(message => {
+        const {sender, receiver, content} = message.payload;
+        return (
+          <ListItem key={message.contractId}>
+            <strong>{sender} &rarr; {receiver}:</strong> {content}
+          </ListItem>
+        );
+      })}
     </List>
   );
-}
+};
 
 export default MessageList;
