28c8fcb42832a2113f59d7a7791f45fe9918cfb5
Rohan Jacob-Rao <rohanjr@gmail.com>
2020-03-06 10:42:40 -0500

    Use daml start instead of sandbox and json-api separately

--- ui/src/index.test.tsx
+++ ui/src/index.test.tsx
@@ -5,42 +5,40 @@ import Ledger from '@daml/ledger';
 import { User } from '@daml2ts/create-daml-app/lib/create-daml-app-0.1.0/User';
 import { computeCredentials } from './Credentials';
 
-const DAR_PATH = '.daml/dist/create-daml-app-0.1.0.dar';
 const LEDGER_ID = 'create-daml-app-sandbox';
-const APPLICATION_ID = 'create-daml-app';
 const SANDBOX_PORT = 6865;
 const JSON_API_PORT = 7575;
 
-let sandboxProc: ChildProcess | undefined = undefined;
-let jsonApiProc: ChildProcess | undefined = undefined;
+let startProc: ChildProcess | undefined = undefined;
 
 // Start a fresh sandbox and json api server for each test to have a clean slate
 beforeEach(async () => {
-  // Start processes in create-daml-app root dir
-  // The path should already include '.daml/bin' in the environment where this is run
+  // Run daml process in create-daml-app root dir.
+  // The path should already include '.daml/bin' in the environment where this is run.
   const opts: SpawnOptions = { cwd: '..', stdio: 'inherit' };
 
-  // Start sandbox
-  const sandboxArgs = ['sandbox', '--wall-clock-time', `--port=${SANDBOX_PORT}`, `--ledgerid=${LEDGER_ID}`, DAR_PATH];
-  sandboxProc = spawn('daml', sandboxArgs, opts);
-  await waitOn({resources: [`tcp:localhost:${SANDBOX_PORT}`]});
+  // Use `daml start` to start up the sandbox and json api server.
+  // This is what we recommend to our users (over running the two processes separately),
+  // so we replicate it in these tests.
+  const startArgs = [
+    'start',
+    '--open-browser=no',
+    '--start-navigator=no',
+    '--sandbox-option=--wall-clock-time',
+    `--sandbox-option=--ledgerid=${LEDGER_ID}`,
+  ];
+  startProc = spawn('daml', startArgs, opts);
 
-  // Start JSON API server
-  const jsonApiArgs = ['json-api', '--ledger-host=localhost', `--ledger-port=${SANDBOX_PORT}`, `--http-port=${JSON_API_PORT}`, `--application-id=${APPLICATION_ID}`];
-  jsonApiProc = spawn('daml', jsonApiArgs, opts);
-  await waitOn({resources: [`tcp:localhost:${JSON_API_PORT}`]});
-}, 15_000);
+  // We know that the processes are up and running once their ports become available.
+  await waitOn({resources: [`tcp:localhost:${SANDBOX_PORT}`, `tcp:localhost:${JSON_API_PORT}`]});
+}, 20_000);
 
 afterEach(() => {
-  // Shut down running daml processes
-  // TODO: Test/fix this for windows
-  if (sandboxProc) {
-    sandboxProc.kill("SIGTERM");
-    console.log('Killed sandbox');
-  }
-  if (jsonApiProc) {
-    jsonApiProc.kill("SIGTERM");
-    console.log('Killed JSON API server');
+  // Shut down `daml start` process
+  // TODO: Test/fix this on windows
+  if (startProc) {
+    startProc.kill("SIGTERM");
+    console.log('Killed daml start');
   }
 });
 
